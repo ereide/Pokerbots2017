c65747d0d066662ec51e1ece0a59bf68f55465bc
Eivind Eide <eivind@vikenfiber.no>
2017-01-16 17:23:30 -0500

    Added more funcunality to card and deck class

--- bot/src/deck.cpp
+++ bot/src/deck.cpp
@@ -1,11 +1,45 @@
 #include "deck.hpp"
 
-Deck::Deck()
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using std::vector;
+using std::cerr;
+
+Deck::Deck() : size(0)
 {
-    //ctor
+    for (int i = 0; i < MAX_RANK; i++)
+    {
+        for (int j = 0; j < MAX_SUITS; j++)
+        {
+            Card card(SUITS[j], RANKS[i]);
+            cards.push_back(card);
+            size++;
+        }
+    }
+
+    shuffle();
 }
 
-Deck::~Deck()
+Deck::~Deck() {}
+
+void Deck::shuffle()
 {
-    //dtor
+    size = MAX_SIZE;
+    std::random_shuffle(&cards[0], &cards[MAX_SIZE-1]);
 }
+
+Card Deck::drawCard()
+{
+    if (size == 0)
+    {
+        std::cerr << "ERROR *** DECK EMPTY";
+        Card card(SUITS[0], RANKS[0]);
+        return card;
+    }
+
+    size--;
+    return cards[size];
+}
+
--- bot/src/deck.hpp
+++ bot/src/deck.hpp
@@ -1,17 +1,28 @@
 #ifndef DECK_H
 #define DECK_H
 
+#include "card.hpp"
 #include <vector>
+
+using std::vector;
+
+#define MAX_RANK 13
+#define MAX_SUITS 4
 
 class Deck
 {
-    public:
-        /** Default constructor */
-        Deck();
-        /** Default destructor */
-        virtual ~Deck();
-    protected:
-    private:
+private:
+    static const int MAX_SIZE = 52;
+
+    int size;
+    vector<Card> cards;
+
+public:
+    Deck();
+    ~Deck();
+    Card drawCard();
+    void shuffle();
+    int getDeckSize() const {return size;}
 };
 
-#endif // DECK_H
+#endif
